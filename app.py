b500baee4a12482af39bbe86f6cb651ac8f0a6b3
Hafsaimranazlan <ik9449080@gmail.com>
2024-05-16 04:07:14 -0700

    multiply function added

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from cal_fun import do_addition,do_subtraction
-
+from multiply import do_multiply 
 def main():
     print("welcome to the calculator App")
     print('''\n select the function from the following opition:
           1. Add
-          2. Subtract''')
+          2. Subtract
+          3. multiply ''')
     user_input=input('select the function :')
     x=int(input('value of x :'))
     y=int(input('value of y :'))
@@ -12,7 +13,8 @@ def main():
         result=do_addition(x,y)
     elif user_input == '2':
         result=do_subtraction(x,y)
-    print(result)
+    elif user_input == '3':
+        result=do_multiply(x,y)
 
 
 if __name__ == "__main__":
---
+++ multiply.py
@@ -0,0 +1,2 @@
+def do_multiply(x,y):
+    return x*y
